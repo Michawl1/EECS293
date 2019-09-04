47a1789848c9f4d7a48cbc2787f266b31185255d
Michael Thompson <mjt106@case.edu>
2019-09-04 10:43:31 -0400

    Starting project, nothing really of value however

---
+++ Assignment1/src/Assignment1.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * @file 	Assignment1.java
+ * @author 	Michael Thompson (mjt106)
+ * @date 	9/4/2019
+ * @details	This class outlines assignment 1 for EECS 293
+ ******************************************************************************/
+
+import java.util.List;
+import java.util.Stack;
+
+public class Assignment1 {
+	/*
+	 * @brief The main method of the program
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	/*
+	 * @brief The assignment method for the class
+	 * @param[in] a
+	 * @param[in] b
+	 * @param[in] cmp
+	 */
+	List<Integer> longestSmallerPrefix(List<Integer> a, List<Integer> b) {
+		List<Integer> l = new Stack<Integer>();
+		return l;
+	}
+}
