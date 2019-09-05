3c9bfee57cc7757776be88c965a39e5a716cd7bc
Michael Thompson <mjt106@case.edu>
2019-09-04 21:10:58 -0400

    Adding src files for completed assignment 1

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -5,25 +5,59 @@
  * @details	This class outlines assignment 1 for EECS 293
  ******************************************************************************/
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Stack;
+import java.util.ListIterator;
 
 public class Assignment1 {
+	
 	/*
 	 * @brief The main method of the program
 	 */
 	public static void main(String[] args) {
+		List<Integer> a = Arrays.asList(1, 2, 2, 3);
+		List<Integer> b = Arrays.asList(1, 2, 3);
+		Comparator<Integer> comp = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer e1, Integer e2){
+				return Integer.compare(e1, e2);
+			}
+		};
+		
+		
+		System.out.println(longestSmallerPrefix(a, b, comp));
 		
 	}
 	
 	/*
 	 * @brief The assignment method for the class
-	 * @param[in] a
-	 * @param[in] b
-	 * @param[in] cmp
-	 */
-	List<Integer> longestSmallerPrefix(List<Integer> a, List<Integer> b) {
-		List<Integer> l = new Stack<Integer>();
-		return l;
+	 * @param[in] a : A List of any type
+	 * @param[in] b : A List of the same type as @a
+	 * @param[in] cmp : A comparator that implements compare(arg1, arg2) for the type of @a and @b
+	 */	
+	static <T> List<T> longestSmallerPrefix(List<T> a, List<T> b, Comparator<? super T> cmp) {
+		ListIterator<T> aIterator = a.listIterator();
+		ListIterator<T> bIterator = b.listIterator();
+		
+		List<T> returnList = new ArrayList<T>();
+		
+		T tempA = null;
+		T tempB = null;
+		
+		while(aIterator.hasNext() && bIterator.hasNext()) {
+			tempA = aIterator.next();
+			tempB = bIterator.next();
+			
+			if(cmp.compare(tempA, tempB) <= 0) {
+				returnList.add(tempA);
+			}
+			else {
+				break;
+			}	
+		}
+		
+		return returnList;
 	}
 }
