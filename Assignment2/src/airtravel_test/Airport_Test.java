58032e8d75d0764268c92166374be88dd72e0f3f
Michael Thompson <mjt106@case.edu>
2019-09-11 18:59:37 -0400

    Adding unit testing stubs

---
+++ Assignment2/src/airtravel_test/Airport_Test.java
@@ -0,0 +1,14 @@
+package airtravel_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Airport_Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Assignment2/src/airtravel_test/SimpleFlight_Test.java
@@ -0,0 +1,14 @@
+package airtravel_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleFlight_Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
